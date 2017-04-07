9a25fdb51f93ee2626d3b1c25e8fef4414e76f56
Martin Hoefling <martin.hoefling@gmx.de>
2017-04-07 14:52:23 +0200

    fixed task

--- init.groovy
+++ init.groovy
@@ -1,5 +1,5 @@
 pipelineJob('pipeline2') {
-    displayName('Pipeline 2')
+    displayName('Pipeline 2 (master/stable)')
     parameters {
         choiceParam('BRANCH', ['master', 'stable'])
     }
@@ -15,6 +15,40 @@ pipelineJob('pipeline2') {
                 }
             }
             scriptPath('pipelines/pipeline2')
+        }
+    }
+}
+
+pipelineJob('pipeline1') {
+    displayName('Pipeline 1')
+    parameters {
+        choiceParam('BRANCH', ['master', 'stable'])
+    }
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        name('origin')
+                        url('https://github.com/martinhoefling/jenkinspipelines.git')
+                    }
+                    branch('*/${BRANCH}')
+                }
+            }
+            scriptPath('pipelines/pipeline1')
+        }
+    }
+}
+
+multibranchPipelineJob('Jenkinsfile Pipeline') {
+    branchSources {
+        git {
+            remote('https://github.com/martinhoefling/jenkinspipelines.git')
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
         }
     }
 }
